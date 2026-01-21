80f3a041efb4c48874a0f9184fc9c661058e859e
114961062-lab <114961062@g.nccu.edu.tw>
2026-01-21 10:14:28 +0800

    Create exam.js

---
+++ exam.js
@@ -0,0 +1,6 @@
+// ==========================================
+// exam.js - 司律模組統一入口
+// ==========================================
+export * from './exam-core.js';
+export * from './exam-judge.js';
+export * from './exam-lawyer.js';
